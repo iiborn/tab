ac5597a9b04d138883a1d5d7bc7c55dac8836dab
iiborn <abbasgit91@gmail.com>
2017-03-19 16:46:15 +0000

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,6 +2,6 @@
 COUNTER=1
 while(true) do
 ./tabchi*.sh
-curl "https://api.telegram.org/bot232820139:AAFnJNel5eS2dmCL-8uT4dRFfs2ABUhwgE4/sendmessage" -F "chat_id=-1001093510408" -F "text=#NEWCRASH-#Tabchi-Reloaded-${COUNTER}-times"
+curl "https://api.telegram.org/bot232820139:AAFnJNel5eS2dmCL-8uT4dRFfs2ABUhwgE4/sendmessage" -F "chat_id=1001093510408" -F "text=#NEWCRASH-#Tabchi-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
