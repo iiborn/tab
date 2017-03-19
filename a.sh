ca2d0aa5d32dd457374ed5f54647bef8f83465db
iiborn <abbasgit91@gmail.com>
2017-03-19 16:43:21 +0000

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNTER=1
 while(true) do
-./beyond.sh
-curl "https://api.telegram.org/bot232820139:AAFnJNel5eS2dmCL-8uT4dRFfs2ABUhwgE4/sendmessage" -F "chat_id=-1001093510408" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
+./tabchi*.sh
+curl "https://api.telegram.org/bot232820139:AAFnJNel5eS2dmCL-8uT4dRFfs2ABUhwgE4/sendmessage" -F "chat_id=-1001093510408" -F "text=#NEWCRASH-#Tabchi-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
